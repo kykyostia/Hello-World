4f924921bec0781d545c775f76f54c58609b0b81
Kylah Ostia <kykyostia11@gmail.com>
2025-01-17 16:09:48 +0800

    adding es6

---
+++ script.js
@@ -0,0 +1,19 @@
+function (MyFucntion ) () {
+   alert=  "i AM AN ALERT BOX"
+}
+
+<script>
+// Create an Object
+const person = 
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue";
+
+// Destructuring Assignment
+let { firstName, age } = person;
+
+// Display Data
+document.getElementById("demo").innerHTML =
+firstName + " is " + age + " years old.";
+</script>
